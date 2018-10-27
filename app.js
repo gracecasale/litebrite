adc3d3f38901d85094ab02c757fa12e09ccd1174
Grace Casale <gracecasale@gmail.com>
2018-10-26 20:36:17 -0400

    updated JS to reload page on click of reset button

--- app.js
+++ app.js
@@ -4,18 +4,27 @@
 const board = document.querySelector('.board');
 const cells = board.querySelectorAll('.cell');
 const colorSelector = document.querySelector('.color-selector');
-const controls = document.querySelector('.control');
+const resetBtn = document.querySelector('.reset');
 let currentColor = 'black';
+
  
 //events
+resetBtn.addEventListener('click', reset);
+
 cells.forEach(function attachCellListener(cell){
     cell.addEventListener('click', onCellClick); 
 }); 
- 
+
+
 //functions
 function onCellClick(event){
     const element = event.target;
     element.style.backgroundColor = currentColor;
 }
- 
+
+function reset (event) {
+    location.reload();
+}
+
+
 //kick it off
